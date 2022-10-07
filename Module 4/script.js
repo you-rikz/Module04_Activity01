8ac23fb95a94d580781d719072b83e29dda8d8ef
Your Name <you@example.com>
2022-10-07 16:52:30 +0800

    first commit

---
+++ Module 4/script.js
@@ -0,0 +1,21 @@
+//Activity 1 in javascript
+function msg (){
+
+    alert("This is the External Script Tag");
+}
+
+function msgVar(){
+    let msg="Welcome!";
+    let user ="Arnold";
+    alert(msg);
+        alert(user);
+}
+//This is the the terms
+function vbTerm(){
+    let a ="Service Ace",
+        b ="Monster Block",
+        c = "Spike",
+        d = "Power Tip";
+
+        alert(`These are some Volleyball terms: ${a}, ${b}, ${c}, ${d}`);
+}
